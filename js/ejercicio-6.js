a2cec78ea410d00dccbf86413c8f6e519abbfd3c
Corina <corinajimenez061102@gmail.com>
2025-09-21 23:13:50 -0300

    agregue el script para el ejercicio 6 que calcula el perimetro de un rectangulo

---
+++ js/ejercicio-6.js
@@ -0,0 +1,20 @@
+/*6- Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo, 
+luego crear una función para calcular su perímetro y mostrarlo por pantalla.
+
+La fórmula del perímetro  es p = 2*(a +b)
+Ejemplo:
+Input:
+lado A = 24
+lado B = 5
+Output: 58
+ */
+
+function perimetro(){
+    const a = parseFloat(prompt('Ingrese el valor de un lado del rectangulo:'))
+    const b = parseFloat(prompt('Ingrese el valor del otro lado del rectangulo:'))
+    const p = 2 * (a + b)
+    return p
+}
+
+const resultado = perimetro()
+document.writeln(`<p> El perimetro del rectangulo es: ${resultado}</p>`)
